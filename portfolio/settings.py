3082dac494be6539dec546dfcce9823aa8691be9
abhay-yadav01 <abhayyadav19110@gmail.com>
2025-06-10 11:27:42 +0530

    Ready with SQLite and safe settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -75,12 +75,8 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'your_db_name',
-        'USER': 'your_user_name',
-        'PASSWORD': 'your_password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
