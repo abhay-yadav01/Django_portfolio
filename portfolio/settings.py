cb4830a95b1d81ebf6c870fda959abe724dba73b
abhay-yadav01 <abhayyadav19110@gmail.com>
2025-06-16 22:05:19 +0530

    modified:portfolio/settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -10,8 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-import os
-import dj_database_url
 from pathlib import Path 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,12 +75,10 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': 
-    dj_database_url.config(default=os.getenv('DATABASE_URL'))
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
